3df0f09c76bb5f444634f19780915a576cf9cf34
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-04 11:43:31 +0200

    08_dictionary_Comprehensions

--- 08_dictionary_Comprehensions/script.py
+++ 08_dictionary_Comprehensions/script.py
@@ -1,2 +1,8 @@
 drinks = ["espresso", "chai", "decaf", "drip"]
 caffeine = [64, 40, 0, 120]
+
+drinks_to_caffeine  = {key: value for key, value in zip(drinks, caffeine)}
+
+#zipped_drinks = zip(drinks, caffeine)
+
+print(drinks_to_caffeine )
