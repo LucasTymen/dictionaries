c5db1030ef1ef3356178378ce5416030d2b3bccb
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-04 10:43:08 +0200

    04_empty_dictionary

---
+++ 04_empty_dictionary/theory.py
@@ -0,0 +1,25 @@
+"""
+Creating Dictionaries
+Empty Dictionary
+
+A dictionary doesn’t have to contain anything. Sometimes we need to create an empty dictionary when we plan to fill it
+later based on some other input.
+
+We can create an empty dictionary like this:
+"""
+empty_dict = {}
+"""
+We will explore ways to fill a dictionary in the next exercise.
+Instructions
+1.
+
+Create an empty dictionary called my_empty_dictionary.
+
+It should look something like:
+"""
+name = {}
+"""
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+
+"""
